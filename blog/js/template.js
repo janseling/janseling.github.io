9bfe075d2cc2867e7a2018cce80673134281d49c
Max Jane <max@quan0.com>
2017-01-01 18:08:29 +0800

    blog 动态加载header和footer

---
+++ blog/js/template.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('#header').load('../header.html', function () {
+        console.log(args);
+    });
+
+    $('#footer').load('../footer.html', function () {
+        console.log(args);
+    });
+});
