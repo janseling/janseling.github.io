a4781fcb7eb639803010eb53caaedab6425d640d
Max Jane <max@quan0.com>
2017-01-31 15:52:40 +0800

    公用模板 header

--- js/template.js
+++ js/template.js
@@ -1,5 +1,4 @@
 $(function () {
-    MLoader.load($('#nav'), '/template/nav.html?'+ new Date().getTime(), false);
     MLoader.load($('#header'), '/template/header.html?'+ new Date().getTime(), false);
     MLoader.load($('#footer'), '/template/footer.html?'+ new Date().getTime(), false);
 });
